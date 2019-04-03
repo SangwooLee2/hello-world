883c18f4b02c4e539c8140f4b226a99ece984ab1
SangwooLee <lsw00kor@gmail.com>
2019-04-03 15:26:23 -0400

    triangle.py added

---
+++ triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 10:09:56 2019
+
+@author: sangwoolee
+"""
+
+import turtle# a module
+painter = turtle.Turtle()
+
+for _ in range(3):
+    painter.forward(100)    
+    painter.left(120) # turns 90 degress
+    
+
+turtle.done()   # stop execution
