416b1e21959c3f9539da0a7b3b994ea84597174e
SangwooLee <lsw00kor@gmail.com>
2019-04-03 12:14:36 -0400

    add scripts

---
+++ draw.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 10:09:56 2019
+
+@author: sangwoolee
+"""
+
+import turtle# a module
+painter = turtle.Turtle()
+
+for _ in range(18):
+    painter.forward(100)    
+    painter.right(100) # turns 90 degress
+
+turtle.done()   # stop execution
